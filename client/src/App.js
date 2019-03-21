01b004df4787cab3d492b5e30cd103deaf1877bf
Drew <drewharper29@gmail.com>
2019-03-21 13:13:07 -0400

    added redux store and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route  } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware,compose} from 'redux';
+import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
+const middleware = [thunk];
+const store = createStore(rootReducer,{},compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 class App extends Component {
   render() {
     return (
+        <Provider store={store}>
         <Router>
       <div className="App">
 
@@ -20,6 +27,7 @@ class App extends Component {
         <Footer />
       </div>
       </Router>
+      </Provider>
     );
   }
 }
